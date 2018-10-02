deaf5b8f00fcb41f218bd03a5063ee0feeb98ff9
Pablo Bonilla <pbonilla29@hotmail.com>
2018-10-02 15:46:53 -0600

    Added UI to Templates

---
+++ Templates/UI.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class UI {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintStream out = System.out;
+
+    public static void main(String[] args) throws IOException {
+        menuPrincipal();
+    }
+
+    public static void menuPrincipal() throws IOException {
+        String opcion;
+        boolean menu = true;
+        do {
+            mostrarMenu();
+            opcion = leerOpcion();
+            menu = seleccionarOpcion(opcion);
+        } while (menu);
+    }
+
+    public static String leerOpcion() throws IOException {
+        out.println("----------------------------------------");
+        out.print("Seleccione una opción: ");
+        String opcion = br.readLine();
+        return opcion;
+    }
+
+    public static void mostrarMenu() {
+        out.println("\n----------------------------------------");
+        out.println("1. Primera opción");
+        out.println("2. Segunda opción");
+        out.println("3. Detener la aplicación");
+    }
+
+    public static void detenerAplicacion() {
+        out.println("[!] Deteniendo la aplicación...");
+        System.exit(0);
+    }
+
+    public static boolean seleccionarOpcion(String opcion) {
+        boolean menu = true;
+
+        if (opcion.equals("1")) {
+            // Primera opción
+        }
+
+        if (opcion.equals("2")) {
+            // Segunda opción
+        }
+
+        if (opcion.equals("3")) {
+            detenerAplicacion();
+        }
+
+        return menu;
+    }
+}
